fab8746ec1c00cdc3fc450f909e9676c96fe00f4
tsudweeks <tayvensudweeks24@gmail.com>
2022-11-29 16:16:11 -0700

    Start project

--- app/src/main/java/com/example/godsofthebeyond/Armor.java
+++ app/src/main/java/com/example/godsofthebeyond/Armor.java
@@ -2,6 +2,16 @@ package com.example.godsofthebeyond;
 
 public class Armor {
     private int physDef, magDef, evade;
+    private String jobReq;
+
+    Armor(String armorName){
+        if(armorName == ""){
+            this.physDef = 4;
+            this.magDef = 2;
+            this.evade = 5;
+            this.jobReq = "";
+        }
+    }
 
     public int getEvade() {
         return evade;
--- app/src/main/java/com/example/godsofthebeyond/MainActivity.java
+++ app/src/main/java/com/example/godsofthebeyond/MainActivity.java
@@ -3,19 +3,46 @@ package com.example.godsofthebeyond;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public String options;
+    public String input;
+    EditText etInput;
+    TextView output, tvOptions, char1, char2, char3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        OnInit();
+
     }
 
+    public void OnInit(){
+        output = findViewById(R.id.tvOutput);
+        tvOptions = findViewById(R.id.tvOptions);
+        char1 = findViewById(R.id.tvChar1);
+        char2 = findViewById(R.id.tvChar2);
+        char3 = findViewById(R.id.tvChar3);
+        etInput = findViewById(R.id.etInput);
+    }
 
+    public void gameStart(){
+
+        output.setText("You find yourself in a massive church, covered in haunting markings");
+        tvOptions.setText("Knight, Alchemist, Hunter, Grappler, Mercenary");
+
+    }
+
+    public void SubmitText(View view){
+        input = etInput.getText().toString().toLowerCase();
+        currentEvent();
+    }
     
 }
