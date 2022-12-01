f7f23050d35638d287cc2d1be0edbef18866215a
xbrennan <chompyman101@gmail.com>
2022-11-30 21:40:08 -0700

    Yeet

--- app/src/main/java/com/example/godsofthebeyond/Game.java
+++ app/src/main/java/com/example/godsofthebeyond/Game.java
@@ -4,15 +4,40 @@ public class Game {
 
     Character[] characters = new Character[3];
     Database database = new Database();
+    int states = 1;
+    int characterIndex = 0;
     int gameProgress = 0;
 
-    public void setCharacters(String name, String job, int characterID){
+    public String gameText(){
 
         if(gameProgress == 0){
 
-            characters[characterID] = new Character(name, job);
+            if(characterIndex == 0){
+
+                if(states == 1){
+
+                    return "Your group is made up of ragtag individuals on their last hope, working for the king of Tharath. " +
+                            "Your mission now is to hunt down the cultists that have recently taken up residence in one of the abandoned churches of Astoth. " +
+                            "This is a bit worrisome, as Astoth was an eldritch horror from ages past, but the king is sure you lot can handle it before it gets worse. " +
+                            "Speaking of, who makes up your group? Let's start with the first. What was their name?";
+
+                }
+
+            }
 
         }
+
+    }
+
+    public String choicesText(){
+
+
+
+    }
+
+    public void setCharacters(String name, String job){
+
+            characters[characterIndex] = new Character(name, job);
 
     }
 
